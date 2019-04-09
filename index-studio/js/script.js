c5542c3efb909e1dca77f15a502265d4c0067e4f
ferzb <vorynkavetal@gmail.com>
2019-04-09 10:10:50 +0300

    Add files via upload

--- index-studio/js/script.js
+++ index-studio/js/script.js
@@ -1,38 +1,50 @@
 $(document).ready(function() {
 	$(function(){
-		$(window).scroll(function() {
+		if ($(window).width() > 640) {
+			var main_number = $('.main_box').length;
+			var main_width = $('.main_box').width();
+			var last_section = $('.section_content').width();
+			$('.main_scroll').css('width', main_width*main_number+last_section+'px')
+			var scroll_box_width = $('.main_scroll').width();
+			var window_h = $(window).height();
+			$('body').css('height', scroll_box_width / window_h*565 + 'px');
 			
-		});
+			scrolling ();
+		}
 	})
-	window.onscroll = function (e) {
-		var valueTop = window.scrollY;
-		$('.main_scroll').css('transform', 'translateX(-'+valueTop/2+'px)');
-		$('.ball').css('transform', 'translateX(-'+valueTop/18+'px)');
-		//$('.general-title').css('transform', 'translateX('+valueTop/21+'px)');
-		$('.triangle').css('transform', 'translateX('+valueTop/20+'px) rotate(-75deg)');
-		$('.squer').css('transform', 'rotate('+valueTop/20+'deg)');
-		//$('.wrapper').css('transform', 'translateX('+valueTop/16+'px)');
+
+
+	function scrolling (){
+		window.onscroll = function () {
+			var valueTop = window.scrollY;
+			$('.main_scroll').css('transform', 'translateX(-'+valueTop+'px)');
+
+			$('.ball').css('transform', 'translateX(-'+valueTop/10+'px)');
+
+			$('.triangle').css('transform', 'translateX('+valueTop/11+'px) rotate(-75deg)');
+
+			$('.squer').css('transform', 'rotate('+valueTop/10+'deg)');
+		}
 	};
 
-	var main_number = $('.main_box').length;
-
-	var last_section = $('.section_content').width();
-
-	$('.main_scroll').css('width', 'calc( ' + 100*main_number + '% + ' + last_section + 'px)')
 	
-	var scroll_box_width = $('.main_scroll').width();
-
-	$('body').css('height', 'calc('+scroll_box_width*2 +'px');
 
 	$(window).resize(function() {
-		var main_number = $('.main_box').length;
+		if ($(window).width() > 640) {
+			var main_number = $('.main_box').length;
+			var main_width = $('.main_box').width();
+			var last_section = $('.section_content').width();
+			$('.main_scroll').css('width', main_width*main_number+last_section+'px')
+			var scroll_box_width = $('.main_scroll').width();
 
-		var last_section = $('.section_content').width();
+			var window_h = $(window).height();
+			$('body').css('height', scroll_box_width / window_h*565 + 'px');
 
-		$('.main_scroll').css('width', 'calc( ' + 100*main_number + '% + ' + last_section + 'px)')
-		
-		var scroll_box_width = $('.main_scroll').width();
-
-		$('body').css('height', 'calc('+scroll_box_width*2 +'px - 8vw');
+			scrolling ();
+			
+		} else(
+			$('.main_scroll').css('width', '100%'),
+			$('body').css('height', '100%')
+		)
 	});
 });
