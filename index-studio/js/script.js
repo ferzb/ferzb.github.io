4be69c516b1ef178eef4cae2b7a775ea2d7fa2da
ferzb <vorynkavetal@gmail.com>
2019-04-13 17:05:44 +0300

    commit

--- index-studio/js/script.js
+++ index-studio/js/script.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
 	$(function(){
 		if ($(window).width() > 640) {
-			var main_number = $('.main_box').length;
-			var main_width = $('.main_box').width();
-			var last_section = $('.section_content').width();
-			$('.main_scroll').css('width', main_width*main_number+last_section+'px')
-			var scroll_box_width = $('.main_scroll').width();
-			var window_h = $(window).height();
-			$('body').css('height', scroll_box_width / window_h*565 + 'px');
-			
+			$.jInvertScroll(['.main_scroll'], {
+				height: 'auto',
+				onScroll: function(percent) {
+				}
+			});
 			scrolling ();
 		}
 	})
@@ -17,47 +14,24 @@ $(document).ready(function() {
 	function scrolling (){
 		window.onscroll = function () {
 			var valueTop = window.scrollY;
-			$('.main_scroll').css('transform', 'translateX(-'+valueTop+'px)');
-
 			$('.ball').css('transform', 'translateX(-'+valueTop/10+'px)');
-
 			$('.triangle').css('transform', 'translateX('+valueTop/11+'px) rotate(-75deg)');
-
 			$('.squer').css('transform', 'rotate('+valueTop/10+'deg)');
 		}
 	};
 	$(function() {
 		$(window).scroll(function(){
 			var window_offset_top = $(window).scrollTop();
-			if (window_offset_top >= 1450) {
+			if (window_offset_top >= 1480) {
 				$('.header_logo').css('color', '#fff')
 			} else(
 				$('.header_logo').removeAttr('style')
 			)
-			if (window_offset_top >= 2400) {
+			if (window_offset_top >= 2700) {
 				$('.nav-icon_line').css('background', '#fff')
 			} else(
 				$('.nav-icon_line').removeAttr('style')
 			)
 		})
 	})
-
-	$(window).resize(function() {
-		if ($(window).width() > 640) {
-			var main_number = $('.main_box').length;
-			var main_width = $('.main_box').width();
-			var last_section = $('.section_content').width();
-			$('.main_scroll').css('width', main_width*main_number+last_section+'px')
-			var scroll_box_width = $('.main_scroll').width();
-
-			var window_h = $(window).height();
-			$('body').css('height', scroll_box_width / window_h*565 + 'px');
-
-			scrolling ();
-			
-		} else(
-			$('.main_scroll').css('width', '100%'),
-			$('body').css('height', '100%')
-		)
-	});
 });
