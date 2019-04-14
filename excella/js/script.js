dfea386e0d511384508e21c104986ecd78c29d4f
ferzb <vorynkavetal@gmail.com>
2019-04-14 15:51:05 +0300

    commit

--- excella/js/script.js
+++ excella/js/script.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
-	$('.header_logo, .main_header, .nav-icon, .header_info, .main_about').addClass('show');
+	$('.header_logo, .main_header, .nav-icon, .header_info, .main_about, .navbar').addClass('show');
 
 	var toggleBlock = $('.nav-icon'),
 		toggleLine = $('.nav-icon_line'),
-		nav = $('.navbar');
+		nav = $('.navbar'),
+		links = $('.home_navbar').find('a');
 
 	toggleBlock.on('click', function(event) {
 		toggleLine.toggleClass('active');
 		nav.slideToggle();
 	});
+
+	links.on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+		nav.removeAttr('style');
+		toggleLine.removeClass('active');
+    });
 
 	$(window).resize(function() {
 		if ($(window).width() > 640) {
