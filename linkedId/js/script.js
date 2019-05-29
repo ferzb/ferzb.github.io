3468e061bb9bab2d45cc8894cbf7f5420dedd30d
ferzb <vorynkavetal@gmail.com>
2019-05-29 21:13:07 +0300

    qwe

--- linkedId/js/script.js
+++ linkedId/js/script.js
@@ -3,18 +3,37 @@
 $(document).ready(function() {
 
 	let g_dropdown = $('.general_dropdown'),
-		g_dropdown_box = $('.dropdown_box');
+		g_dropdown_box = $('.dropdown_box'),
+		close = $('.close');
 	
+	close.click(function(){
+		g_dropdown_box.removeClass('open');
+		$('.all_filters_box').removeClass('show');
+		$('.overaly').removeAttr('style');
+		$('html, body').removeAttr('style');
+	})
+
 	g_dropdown.each(function(){
 		let that = $(this),
 			dropdown_box = that.find('.dropdown_box'),
 			drop_btn = that.find('.drop_btn');
 
 		drop_btn.click(function(){
-			g_dropdown_box.removeClass('open');
 			$(this).toggleClass('active');
 			dropdown_box.toggleClass('open');
 		})
+	})
+
+	$('.toggle_all_filters').click(function(){
+		$('.all_filters_box').addClass('show');
+		$('.overaly').css('display', 'block');
+		$('html, body').css('overflow', 'hidden');
+	})
+
+	$('.overaly').click(function(){
+		$('.all_filters_box').removeClass('show');
+		$(this).removeAttr('style');
+		$('html, body').removeAttr('style');
 	})
 
 	
