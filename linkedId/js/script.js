a7404eea93693ac405d4ddad6b58b1d842448156
ferzb <vorynkavetal@gmail.com>
2019-05-29 20:10:50 +0300

    qwe

--- linkedId/js/script.js
+++ linkedId/js/script.js
@@ -2,7 +2,22 @@
 
 $(document).ready(function() {
 
-    
+	let g_dropdown = $('.general_dropdown'),
+		g_dropdown_box = $('.dropdown_box');
+	
+	g_dropdown.each(function(){
+		let that = $(this),
+			dropdown_box = that.find('.dropdown_box'),
+			drop_btn = that.find('.drop_btn');
+
+		drop_btn.click(function(){
+			g_dropdown_box.removeClass('open');
+			$(this).toggleClass('active');
+			dropdown_box.toggleClass('open');
+		})
+	})
+
+	
     //input focus function
     let input_box = $('.input_box input');
     
