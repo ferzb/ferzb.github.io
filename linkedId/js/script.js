bee53b293152e1815b925d3cf21e0452466a6aa7
ferzb <vorynkavetal@gmail.com>
2019-06-01 10:49:18 +0300

    qwe

--- linkedId/js/script.js
+++ linkedId/js/script.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
 
 	let g_dropdown = $('.general_dropdown'),
 		g_dropdown_box = $('.dropdown_box'),
-		close = $('.close');
+		close = $('.close'),
+		all_filters_box = $('.all_filters_box');
 	
 	close.click(function(){
 		g_dropdown_box.removeClass('open');
-		$('.all_filters_box').removeClass('show');
+		all_filters_box.removeClass('show');
 		$('.overaly').removeAttr('style');
 		$('html, body').removeAttr('style');
 	})
@@ -25,17 +26,48 @@ $(document).ready(function() {
 	})
 
 	$('.toggle_all_filters').click(function(){
-		$('.all_filters_box').addClass('show');
+		all_filters_box.addClass('show');
 		$('.overaly').css('display', 'block');
 		$('html, body').css('overflow', 'hidden');
 	})
 
 	$('.overaly').click(function(){
-		$('.all_filters_box').removeClass('show');
+		all_filters_box.removeClass('show');
 		$(this).removeAttr('style');
 		$('html, body').removeAttr('style');
 	})
 
+
+	all_filters_box.each(function(){
+		let that = $(this),
+			checkbox = that.find('input'),
+			clear_btn = that.find('.clear_btn'),
+			check_number = clear_btn.find('span')
+			
+
+		checkbox.click(function(){
+
+			let checkbox_checked_l = that.find('input:checked').length;
+
+			check_number.each(function(){
+				if(checkbox_checked_l < 1){
+					check_number.html(' ');
+					clear_btn.attr('data-status', 'disable')
+				}else{
+					check_number.html(checkbox_checked_l),
+					clear_btn.attr('data-status', 'able')
+				}
+			})
+
+		})
+
+		clear_btn.click(function(){
+			$('input:checked').prop('checked', false);
+			check_number.html(' ');
+			$(this).attr('data-status', 'disable')
+		})
+			
+	})
 	
     //input focus function
     let input_box = $('.input_box input');
