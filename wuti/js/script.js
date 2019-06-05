c5cf320e9568f054f1342d90f6a21ba3b03280be
ferzb <vorynkavetal@gmail.com>
2019-06-05 21:34:06 +0300

    qwe

--- wuti/js/script.js
+++ wuti/js/script.js
@@ -2,6 +2,32 @@
 
 $(document).ready(function() {
 
+	//navbar func
+
+	let nav_icon = $('.nav-icon'),
+		navbar = $('.navbar');
+
+	nav_icon.click(function(){
+		$(this).toggleClass('active');
+		navbar.slideToggle();
+
+		if($(this).hasClass('active')){
+			
+			$('html, body').css({
+				'overflow': 'hidden'
+			})
+		}else{
+			$('html, body').removeAttr('style')
+		}
+	})
+
+	$(window).resize(function() {
+		if ($(window).width() > 780) {
+			navbar.removeAttr('style');
+			nav_icon.removeClass('active');
+			$('html, body').removeAttr('style');
+		}
+	});
     
     //input focus function
     let input_box = $('.input_box input');
